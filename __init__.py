22dd7c258c785abd1c7a0983e8311dd5b2c4c7c8
InKyung H <red131729@khu.ac.kr>
2022-05-23 00:38:42 +0900

    commit for test

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from api import get_all_openapi_data
-
-if __name__ == "__main__":
-    res = get_all_openapi_data()
-    print(res)
--- api_prac/__init__.py
+++
@@ -1,6 +0,0 @@
-from api import get_all_openapi_data
-
-if __name__ == "__main__":
-    print(__name__)
-    # res = get_all_openapi_data()
-    # print(res)

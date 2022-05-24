1e97f941917a21cf66e329082ac6ce5b38a26189
InKyung H <red131729@khu.ac.kr>
2022-05-25 04:58:51 +0900

    select columns in data

---
+++ spark.py
@@ -0,0 +1,7 @@
+from pyspark.sql import *
+
+spark = SparkSession.builder.appName("Python Spark SQL basic example").config("spark.some.config.option").getOrCreate()
+
+iris_df = spark.read.csv("test/iris.csv", inferSchema=True, header=True)
+
+iris_df.show(5)

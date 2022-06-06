c72fb7dea368cc1cde7455f99004c85e6e28b63c
InKyung H <red131729@khu.ac.kr>
2022-06-07 07:19:35 +0900

    fix typo

--- spark/spark/spark_configure.py
+++ spark/spark/spark_configure.py
@@ -71,13 +71,10 @@ class SparkS3(object):
 
     def send_file(self, save_df_result: DataFrame, key: str):
         save_df_result.write.format("org.apache.spark.sql.json").mode("append").save(f"s3://{self.send_bucket}/{key}")
+        save_df_result.write.format("json").save(f"s3://{self.send_bucket}/test_{key}")
         try:
-            while True:
-                exists = save_df_result.columns - list(set(save_df_result.columns))
-                if not exists:
-                    break
-                for ex in exists:
-                    save_df_result = save_df_result.drop(save_df_result[ex])
+            while "trdar_cd" in save_df_result.columns:
+                save_df_result = save_df_result.drop(save_df_result["trdar_cd"])
 
             df = save_df_result.repartition(1).toJSON().map(lambda x: json.loads(x)).collect()
             self.s3_client.put_object(

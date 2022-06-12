2fe55f3d80e7d154696b47b6dadb5b60fa386194
InKyung H <red131729@khu.ac.kr>
2022-06-13 04:38:02 +0900

    Delete my_secret.py
    delete iam file

--- spark/utils/my_secret.py
+++
@@ -1,6 +0,0 @@
-profile_info = {
-    "aws_access_key_id": "AKIAZJ5G7MQHGFJITW6Z",
-    "aws_secret_access_key": "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",
-    "region": "us-east-1",
-    "aws_bucket_name": "rtc-raw-data",
-}
